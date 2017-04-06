cd93df9e2d5e697b97845d876310e1d8af8e3467
jeanslee <sbnui2004@126.com>
2017-04-06 10:38:22 +0800

    second commit

--- sh.sh
+++ sh.sh
@@ -1,7 +1,7 @@
 #echo "# OpenWrt-SDK-ar71xx-for-linux-i686-gcc-4.8-linaro_uClibc-0.9.33.2" >> README.md
 git init
 git add .
-git commit -m "first commit"
+git commit -m "second commit"
 #git remote add origin https://github.com/jeanslee/OpenWrt-SDK-ar71xx-for-linux-i686-gcc-4.8-linaro_uClibc-0.9.33.2.git
 #git push -u origin master
 git push
